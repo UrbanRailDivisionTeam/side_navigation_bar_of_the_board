19be602c36aab7e7ec2b29080f0f3bc93aba5df3
cheakf <sunmoming#gmail.com>
2026-06-16 12:48:07 +0800

    fix

--- server/main.py
+++ server/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from side-navigation-bar-of-the-board!")
+from litestar import Litestar
+from litestar.config.cors import CORSConfig
+from litestar.static_files.config import create_static_files_router
 
+cors_config = CORSConfig(allow_origins=["*"], allow_methods=["GET"])
+
+static_files = create_static_files_router(
+    path="/",
+    directories=["static"],
+    name="static",
+    html_mode=True,
+    include_in_schema=False,
+)
+
+app = Litestar(
+    route_handlers=[
+        static_files
+    ],
+    cors_config=cors_config,
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=12375)
